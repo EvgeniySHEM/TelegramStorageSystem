55cf9917cf0e98f4f5a2fcda8f63f5fecec48a42
Evgeniy Sharychenkov <sanctio@yandex.ru>
2023-11-10 20:17:36 +0300

    Реализовал контроллер для скачивания файлов по ссылке

---
+++ rest-service/src/main/java/ru/sanctio/controller/FileController.java
@@ -0,0 +1,68 @@
+package ru.sanctio.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.sanctio.entity.AppDocument;
+import ru.sanctio.entity.AppPhoto;
+import ru.sanctio.entity.BinaryContent;
+import ru.sanctio.service.FileService;
+
+import javax.websocket.server.PathParam;
+
+@Log4j
+@RequestMapping("/file")
+@RestController
+public class FileController {
+
+    private final FileService fileService;
+
+    @Autowired
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/get-doc")
+    public ResponseEntity<?> getDoc(@RequestParam String id) {
+        //TODO для формирования badRequest добавить ControllerAdvice
+        AppDocument document = fileService.getDocument(id);
+        if(document == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        BinaryContent binaryContent = document.getBinaryContent();
+
+        FileSystemResource fileSystemResource = fileService.getFileSystemResource(binaryContent);
+        if (fileSystemResource == null) {
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(document.getMimeType()))
+                .header("Content-disposition", "attachment; filename=" + document.getDocName())
+                .body(fileSystemResource);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/get-photo")
+    public ResponseEntity<?> getPhoto(@RequestParam("id") String id) {
+        //TODO для формирования badRequest добавить ControllerAdvice
+        AppPhoto photo = fileService.getPhoto(id);
+        if (photo == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        BinaryContent binaryContent = photo.getBinaryContent();
+
+        FileSystemResource fileSystemResource = fileService.getFileSystemResource(binaryContent);
+        if (fileSystemResource == null) {
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .header("Content-disposition", "attachment;")
+                .body(fileSystemResource);
+    }
+}

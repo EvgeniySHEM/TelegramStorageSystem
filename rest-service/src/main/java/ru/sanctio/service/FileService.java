4338ae97f8761a49ecea2c78d87b1b8c0fbc2850
Evgeniy Sharychenkov <sanctio@yandex.ru>
2023-11-10 19:54:43 +0300

    Добавил FileServiceImpl

--- rest-service/src/main/java/ru/sanctio/service/FileService.java
+++ rest-service/src/main/java/ru/sanctio/service/FileService.java
@@ -1,4 +1,12 @@
 package ru.sanctio.service;
 
+import org.springframework.core.io.FileSystemResource;
+import ru.sanctio.entity.AppDocument;
+import ru.sanctio.entity.AppPhoto;
+import ru.sanctio.entity.BinaryContent;
+
 public interface FileService {
+    AppDocument getDocument(String id);
+    AppPhoto getPhoto(String id);
+    FileSystemResource getFileSystemResource(BinaryContent binaryContent);
 }

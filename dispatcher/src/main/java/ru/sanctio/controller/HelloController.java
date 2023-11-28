10661c80325b6f49a7deb99c4e88e8ad9dea696a
Evgeniy Sharychenkov <sanctio@yandex.ru>
2023-11-28 19:26:56 +0300

    test p

---
+++ dispatcher/src/main/java/ru/sanctio/controller/HelloController.java
@@ -0,0 +1,14 @@
+package ru.sanctio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        System.out.println();
+        return "Hello World!";
+    }
+}
